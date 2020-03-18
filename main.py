0cffbc0becd986a1a7fffef0eeb567152ff5d6f8
dostoyevsky1 <mdrozdov94@gmail.com>
2020-03-18 14:00:22 -0400

    Introduce exception handling in one_to_one and main modules

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from one_to_one import one_to_one
 
 
 if __name__ == '__main__':
-	s1 = argv[1]
-	s2 = argv[2]
-	one_to_one(s1,s2)
+	try:
+		s1 = argv[1]
+		s2 = argv[2]
+		one_to_one(s1,s2)
+	except IndexError:
+		print('\n' + 'Input must consist of 2 elements' + '\n')
+	except Exception as e:
+		print(e)
+		
+
 	
--- one_to_one.py
+++ one_to_one.py
@@ -1,5 +1,14 @@
 
-def one_to_one(s1: str, s2: str) -> bool:
+
+def one_to_one(s1: str, s2: str) -> None:
+	
+
+	if not isinstance(s1, str) or not isinstance(s2, str):
+		raise TypeError('\n' + 'Input must be a string type' + '\n')
+
+	if len(s1) != len(s2):
+		raise ValueError('\n' + 'Inputs must be of equal length. No isomorphism by default' + '\n')
+
 	try:
 		s1, s2 = s1.lower(), s2.lower()
 		print(len(set(s1)) == len(set(s2)) == len(set(zip(s1,s2))))
@@ -8,4 +17,4 @@ def one_to_one(s1: str, s2: str) -> bool:
 		raise
 
 	return None
-	
+
